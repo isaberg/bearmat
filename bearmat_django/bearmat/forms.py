f98a3c2b1609607dc867829f02ef899e80478d46
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-08-02 15:03:34 -0400

    models complete

--- bearmat_django/bearmat/forms.py
+++ bearmat_django/bearmat/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import broker, Veteran, Business, Search
+from localflavor import generic
 from localflavor.us.forms import USStateField, USZipCodeField
 from django.db import transaction
 
@@ -11,6 +12,7 @@ class VeteranSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+    @transaction.atomic
     def save (self, commit=True):
         user = super().save(commit=False)
         user.is_veteran = True
@@ -25,6 +27,7 @@ class BrokerSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+    @transaction.atomic
     def save (self, commit=True):
         user = super().save(commit=False)
         user.is_veteran = True
@@ -32,32 +35,14 @@ class BrokerSignUpForm(UserCreationForm):
             user.save()
         return user
 
-
-## MUST use this approach, invoked by conditional in views.py
-
-# class StudentSignUpForm(UserCreationForm):
-#     interests = forms.ModelMultipleChoiceField(
-#         queryset=Subject.objects.all(),
-#         widget=forms.CheckboxSelectMultiple,
-#         required=True
-#     )
-#
-#     class Meta(UserCreationForm.Meta):
-#         model = User
-#
-#     @transaction.atomic
-#     def save(self):
-#         user = super().save(commit=False)
-#         user.is_student = True
-#         user.save()
-#         student = Student.objects.create(user=user)
-#         student.interests.add(*self.cleaned_data.get('interests'))
-#         return user
-
-
 # this handles profile creation for veterans
 # model this conditional off of
-class VeteranForm(forms.ModelForm):
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = (first_name, last_name, location_zip, location_state, profile_url, org_name, org_url, education, bio)
+
+class SearchForm(forms.ModelForm):
     state = USStateField()
     zip = USZipCodeField()
 
@@ -65,7 +50,9 @@ class VeteranForm(forms.ModelForm):
         model = Profile
         fields = ('', '')
 
-## example for "student sign up" that I want to use for veteran profile forms
+
+## SAVE this, look at use of queryset and widget for future checkbox in "Search" creation...
+
 # class StudentSignUpForm(UserCreationForm):
 #     interests = forms.ModelMultipleChoiceField(
 #         queryset=Subject.objects.all(),
@@ -84,69 +71,3 @@ class VeteranForm(forms.ModelForm):
 #         student = Student.objects.create(user=user)
 #         student.interests.add(*self.cleaned_data.get('interests'))
 #         return user
-
-
-class brokerForm(forms.ModelForm):
-    state = USStateField()
-    zip = USZipCodeField()
-
-    class Meta:
-        model = Profile
-        fields = ('', '')
-
-
-#Another example below...how to use validator MyFormField()
-# class ArticleForm(ModelForm):
-#     headline = MyFormField(
-#         max_length=200,
-#         required=False,
-#         help_text='Use puns liberally',
-#     )
-#
-#     class Meta:
-#         model = Article
-#         fields = ['headline', 'content']
-
-
-# Localflavor example below...use for US state / zip code validation
-
-# forms.py
-# from django import forms
-# from localflavor.fr.forms import FRPhoneNumberField
-#
-# class MyForm(forms.Form):
-#     my_french_phone_no = FRPhoneNumberField()
-
-
-# class USStateField(Field):
-#     """
-#     A form field that validates its input is a U.S. state name or abbreviation.
-#     It normalizes the input to the standard two-leter postal service
-#     abbreviation for the given state.
-#     """
-#     default_error_messages = {
-#         'invalid': _('Enter a U.S. state or territory.'),
-#     }
-#
-#     def clean(self, value):
-#         from .us_states import STATES_NORMALIZED
-#         super(USStateField, self).clean(value)
-#         if value in EMPTY_VALUES:
-#             return ''
-#         try:
-#             value = value.strip().lower()
-#         except AttributeError:
-#             pass
-#         else:
-#             try:
-#                 return STATES_NORMALIZED[value.strip().lower()]
-#             except KeyError:
-#                 pass
-#         raise ValidationError(self.error_messages['invalid'])
-
-
-# from django import forms
-# from localflavor.fr.forms import FRPhoneNumberField
-#
-# class MyForm(forms.Form):
-#     my_french_phone_no = FRPhoneNumberField()
--- bearmat_django/bearmat/models.py
+++ bearmat_django/bearmat/models.py
@@ -3,16 +3,11 @@ from viewflow.models import Process
 from localflavor.us.models import USZipCodeField, USStateField
 from django.contrib.auth.models import AbstractUser
 
-# validating here rather than mess with CreateView in my View-Form process...
-# from localflavor.us.forms import FRPhoneNumberField
-# localflavor.us.models.USZipCodeField()
-# localflavor.us.models.USStateField()
+# validating here in models rather than mess with CreateView in my View-Form process...
 
 class User(AbstractUser):
     is_veteran = models.BooleanField(default=False)
     is_broker = models.BooleanField(default=False)
-
-# structure...maybe just have a "Profile" that captures veteran/broker info?
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
@@ -25,81 +20,150 @@ class Profile(models.Model):
     org_url = models.TextField(null=True, blank=True)
     education = models.CharField(max_length=280)
     bio = models.CharField(max_length=280)
-    lastUpdated
+    last_update = models.DateField(auto_now=true)
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
 
 class Search(models.Model):
-    veteran = models.ForeignKey(User, on_delete=models.CASCADE, related_name='searches')
-    early = models.
-
-
+    STATE_CHOICES = (
+    ('AL', 'Alabama'),('AK', 'Alaska'),('AZ', 'Arizona'),('AR', 'Arkansas'),
+    ('CA', 'California'), ('CO', 'Colorado'), ('CT', 'Connecticut'),
+    ('DE', 'Delaware'), ('DC', 'District of Columbia'), ('FL', 'Florida'),
+    ('GA', 'Georgia'), ('HI', 'Hawaii'), ('ID', 'Idaho'), ('IL', 'Illinois'),
+    ('IN', 'Indiana'), ('IA', 'Iowa'), ('KS', 'Kansas'), ('KY', 'Kentucky'),
+    ('LA', 'Louisiana'), ('ME', 'Maine'), ('MD', 'Maryland'), ('MA', 'Massachusetts'),
+    ('MI', 'Michigan'), ('MN', 'Minnesota'), ('MS', 'Mississippi'), ('MO', 'Missouri'),
+    ('MT', 'Montana'), ('NE', 'Nebraska'), ('NV', 'Nevada'), ('NH', 'New Hampshire'),
+    ('NJ', 'New Jersey'), ('NM', 'New Mexico'), ('NY', 'New York'), ('NC', 'North Carolina'),
+    ('ND', 'North Dakota'), ('OH', 'Ohio'), ('OK', 'Oklahoma'), ('OR', 'Oregon'),
+    ('PA', 'Pennsylvania'), ('RI', 'Rhode Island'), ('SC', 'South Carolina'),
+    ('SD', 'South Dakota'), ('TN', 'Tennessee'), ('TX', 'Texas'), ('UT', 'Utah'),
+    ('VT', 'Vermont'), ('VA', 'Virginia'), ('WA', 'Washington'), ('WV', 'West Virginia'),
+    ('WI', 'Wisconsin'), ('WY', 'Wyoming')
+    )
+    ADMINISTRATION = 1
+    AGRICULTURE = 2
+    AUTOMOTIVE = 3
+    CONSULTING = 4
+    DEFENSE = 5
+    DESIGN = 6
+    EDUCATION = 7
+    ENERGY = 8
+    EVENTS = 9
+    FINANCE = 10
+    FOOD = 11
+    HEALTH = 12
+    HOSPITALITY = 13
+    INSURANCE = 14
+    LEGAL = 15
+    MANUFACTURING = 16
+    RE = 17
+    RETAIL = 18
+    SERVICES = 19
+    SOFTWARE = 20
+    STORAGE = 21
+    TECHNOLOGY = 22
+    TRADES = 23
+    TRANSPORTATION = 24
+    WASTEMGMT = 25
+    INDUSTRY_CHOICES = (
+    ('ADMINISTRATION', 'Administration'),('AGRICULTURE', 'Agriculture'),
+    ('AUTOMOTIVE', 'Automotive'),('CONSULTING', 'Consulting'),
+    ('DEFENSE', 'Defense'), ('DESIGN', 'Design'), ('EDUCATION', 'Education'),
+    ('ENERGY', 'Energy'), ('EVENTS', 'Events'), ('FINANCE', 'Financial Services'),
+    ('FOOD', 'Food'), ('HEALTH', 'Health Care'), ('HOSPITALITY', 'Tourism & Hospitality'),
+    ('INSURANCE', 'Insurance'), ('LEGAL', 'Legal Services'), ('MANUFACTURING', 'Manufacturing'),
+    ('RE', 'Real Estate'), ('RETAIL', 'Retail'), ('SERVICES', 'Professional & Consumer Services'),
+    ('SOFTWARE', 'Software'), ('STORAGE', 'Storage'), ('TECHNOLOGY', 'Technology'), ('TRADES', 'Trades'),
+    ('TRANSPORTATION', 'Transportation'), ('WASTEMGMT', 'Waste Management')
+    )
+    veteran = models.ForeignKey(auth.User, on_delete=models.CASCADE, related_name='searches')
+    name = models.CharField(max_length=100)
+    early = models.DateField()
+    late = models.DateField()
+    state = models.CharField(
+        choices=STATE_CHOICES,
+        default='MT'
+    )
+    industry = models.CharField(
+        max_length=2,
+        choices=INDUSTRY_CHOICES,
+        default='TRADES'
+    )
+    def __str__(self):
+        return self.name
 
 class Business(models.Model):
+    STATE_CHOICES = (
+    ('AL', 'Alabama'),('AK', 'Alaska'),('AZ', 'Arizona'),('AR', 'Arkansas'),
+    ('CA', 'California'), ('CO', 'Colorado'), ('CT', 'Connecticut'),
+    ('DE', 'Delaware'), ('DC', 'District of Columbia'), ('FL', 'Florida'),
+    ('GA', 'Georgia'), ('HI', 'Hawaii'), ('ID', 'Idaho'), ('IL', 'Illinois'),
+    ('IN', 'Indiana'), ('IA', 'Iowa'), ('KS', 'Kansas'), ('KY', 'Kentucky'),
+    ('LA', 'Louisiana'), ('ME', 'Maine'), ('MD', 'Maryland'), ('MA', 'Massachusetts'),
+    ('MI', 'Michigan'), ('MN', 'Minnesota'), ('MS', 'Mississippi'), ('MO', 'Missouri'),
+    ('MT', 'Montana'), ('NE', 'Nebraska'), ('NV', 'Nevada'), ('NH', 'New Hampshire'),
+    ('NJ', 'New Jersey'), ('NM', 'New Mexico'), ('NY', 'New York'), ('NC', 'North Carolina'),
+    ('ND', 'North Dakota'), ('OH', 'Ohio'), ('OK', 'Oklahoma'), ('OR', 'Oregon'),
+    ('PA', 'Pennsylvania'), ('RI', 'Rhode Island'), ('SC', 'South Carolina'),
+    ('SD', 'South Dakota'), ('TN', 'Tennessee'), ('TX', 'Texas'), ('UT', 'Utah'),
+    ('VT', 'Vermont'), ('VA', 'Virginia'), ('WA', 'Washington'), ('WV', 'West Virginia'),
+    ('WI', 'Wisconsin'), ('WY', 'Wyoming')
+    )
+    ADMINISTRATION = 1
+    AGRICULTURE = 2
+    AUTOMOTIVE = 3
+    CONSULTING = 4
+    DEFENSE = 5
+    DESIGN = 6
+    EDUCATION = 7
+    ENERGY = 8
+    EVENTS = 9
+    FINANCE = 10
+    FOOD = 11
+    HEALTH = 12
+    HOSPITALITY = 13
+    INSURANCE = 14
+    LEGAL = 15
+    MANUFACTURING = 16
+    RE = 17
+    RETAIL = 18
+    SERVICES = 19
+    SOFTWARE = 20
+    STORAGE = 21
+    TECHNOLOGY = 22
+    TRADES = 23
+    TRANSPORTATION = 24
+    WASTEMGMT = 25
+    INDUSTRY_CHOICES = (
+    ('ADMINISTRATION', 'Administration'),('AGRICULTURE', 'Agriculture'),
+    ('AUTOMOTIVE', 'Automotive'),('CONSULTING', 'Consulting'),
+    ('DEFENSE', 'Defense'), ('DESIGN', 'Design'), ('EDUCATION', 'Education'),
+    ('ENERGY', 'Energy'), ('EVENTS', 'Events'), ('FINANCE', 'Financial Services'),
+    ('FOOD', 'Food'), ('HEALTH', 'Health Care'), ('HOSPITALITY', 'Tourism & Hospitality'),
+    ('INSURANCE', 'Insurance'), ('LEGAL', 'Legal Services'), ('MANUFACTURING', 'Manufacturing'),
+    ('RE', 'Real Estate'), ('RETAIL', 'Retail'), ('SERVICES', 'Professional & Consumer Services'),
+    ('SOFTWARE', 'Software'), ('STORAGE', 'Storage'), ('TECHNOLOGY', 'Technology'), ('TRADES', 'Trades'),
+    ('TRANSPORTATION', 'Transportation'), ('WASTEMGMT', 'Waste Management')
+    )
+    broker = models.ForeignKey(auth.User, on_delete=models.CASCADE, related_name='businesses')
     name = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    early = models.DateField()
+    late = models.DateField()
+    state = models.CharField(
+        choices=STATE_CHOICES,
+        default='MT'
+    )
+    industry = models.CharField(
+        max_length=2,
+        choices=INDUSTRY_CHOICES,
+        default='TRADES'
+    )
+    def __str__(self):
+        return self.name
 
-    name:
-city:
-state:
-Search (Record of Business Desired)
-veteran: FOREIGNKEY of originator
-go: (checkbox array of location/industry/$) nogo: (checkbox array of location/industry/$)
-    industry: price: description:
-
-class Veteran(models.Model):
-    # user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    # quizzes = models.ManyToManyField(Quiz, through='TakenQuiz')
-    # interests = models.ManyToManyField(Subject, related_name='interested_students')
-    # This is what the vet model needs:
-    # firstName: lastName: location: service: education:
-    # bio: messages: profileURL:
-
-class Broker(models.Model):
-    # This is what the broker model needs:
-    # firstName:
-    # lastName: profileURL: organization: organizationURL: location:
-
-
-# A one-to-one relationship is quite similar to a many-to-one relationship, except that it restricts two objects to having a unique relationship.
-# An example of this would be a User and a Profile (which stores information about the user). No
-# two users share the same profile. Let's look at this in Django. I won't bother to define the
-# user model, as Django defines it for us. Do note, however, that Django suggests using
-# django.contrib.auth.get_user_model() to import the user, so that's what we'll do.
-# The profile model may be defined as follows:
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(settings.AUTH_USER_MODEL) # Note that Django suggests getting the User from the settings for relationship definitions
-#     fruit = models.CharField(max_length=50, help_text="Favorite Fruit")
-#     facebook = models.CharField(max_length=100, help_text="Facebook Username")
-#
-#     def __unicode__(self):
-#         return "".join(self.fruit, " ", self.facebook)
-
-# class HelloWorldProcess(Process):
-#     text = models.CharField(max_length=150)
-#     approved = models.BooleanField(default=False)
-#
-# class Artist(models.Model):
-#     name = models.CharField(max_length=100)
-#     photo_url = models.TextField()
-#     nationality = models.CharField(max_length=100)
-#
-#     def __str__(self):
-#         return self.name
-#
-#
-# class Song(models.Model):
-#     title = models.CharField(max_length=100)
-#     album = models.CharField(max_length=100)
-#     preview_url = models.TextField(null=True, blank=True)
-#     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
-#
-#     def __str__(self):
-#         return self.title
-#
-#
-# class Favorite(models.Model):
-#     user = models.ForeignKey('auth.User', related_name='favorites', on_delete=models.CASCADE)
-#     song = models.ForeignKey(Song, related_name='favorites', on_delete=models.CASCADE)
+class Favorite(models.Model):
+    veteran = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='favorites')
+    business = models.ForeignKey(Business, related_name='favorites', on_delete=models.CASCADE)
