bd72901e4aa42170c7e59486dce7128c9a2f8544
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-08-03 11:22:19 -0400

    completed migration and running localhost

--- bearmat_django/bearmat/admin.py
+++ bearmat_django/bearmat/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
-admin.site.register(User, UserAdmin)
-
-# the above is allowing me to use the custom User model I am building
+from .models import User, Profile, Search, Business, Favorite
+admin.site.register(User, Search, Business)
